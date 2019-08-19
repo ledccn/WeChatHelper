70269df896d4d315266a99dba149cd000a8f87c7
iyuu.cn <367013672@qq.com>
2019-08-19 20:04:48 +0800

    增强消息回复逻辑

--- Action.php
+++ Action.php
@@ -79,6 +79,7 @@ class WeChatHelper_Action extends Typecho_Widget implements Widget_Interface_Do
      *
      */
     public function postAction(){
+        $options = Helper::options()->plugin('WeChatHelper');
 		$postStr = file_get_contents("php://input");
 		//$this->request->get("HTTP_RAW_POST_DATA");
 
@@ -88,57 +89,116 @@ class WeChatHelper_Action extends Typecho_Widget implements Widget_Interface_Do
 		//@fwrite($file_pointer,$postStr );
 		//@fclose($file_pointer);
 
-        if (!empty($postStr)){
-                $options = Helper::options()->plugin('WeChatHelper');
-                $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
-                $time = time();
-                $msgType = $postObj->MsgType;
-                $keyword = trim($postObj->Content);
-                $cmd = strtolower(substr($keyword, 0, 1));
-                if($msgType == "text" ){
-                    if($cmd=="h" || $cmd=="H"){
-                        $contentStr = "\"n\" 最新日志\n\"r\" 随机日志\n\"l\" 手气不错\n\"s 关键词\" 搜索日志\n\" ";
-                        $resultStr = $this->baseText($postObj, $contentStr);
-                    }elseif ($cmd=="f" || $cmd=="F") {
-                        $resultStr = $this->commentRank($postObj);
-                    }elseif ($cmd=="r" || $cmd=="R") {
-                        $resultStr = $this->randomPost($postObj);
-                    }elseif ($cmd=="n" || $cmd=="N") {
-                        $resultStr = $this->newPost($postObj);
-                    }elseif ($cmd=="l" || $cmd=="L") {
-                        $resultStr = $this->luckyPost($postObj);
-                    }elseif ($cmd=="s" || $cmd=="S") {
-                        $searchParam = substr($keyword, 1);
-                        $resultStr = $this->searchPost($postObj, $searchParam);
-                    }else{
-                    	$info = $keyword ;
-						$resultStr = $this->searchPost($postObj, $searchParam);
-                    }
-                }else if($msgType == "event"){
-                    if($postObj->Event == "subscribe"){
-                        $contentStr = $options->welcome;
-                        $resultStr = $this->baseText($postObj, $contentStr);
-						//$resultStr = $this->newPost($postObj);
-                    }
-					if($postObj->Event == "CLICK"){
-						$eventkey = trim($postObj->EventKey);
-						if ($eventkey=="n" || $eventkey=="N") {
-							$resultStr = $this->newPost($postObj);
-						}
-						if ($eventkey=="l" || $eventkey=="L") {
-							$resultStr = $this->luckyPost($postObj);
-						}
-						if ($eventkey=="r" || $eventkey=="R") {
-							$resultStr = $this->randomPost($postObj);
-						}
-					}
+        if ( $this->checkSignature($options->token) && !empty($postStr) ){
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $time = time();
+            $msgType = $postObj->MsgType;
+            //消息处理流程
+            if($msgType== 'event'){         //事件推送
+                $Event = strtolower($postObj->Event);	//事件类型（转为小写）
+                switch($Event){
+                    case 'subscribe'			:   //订阅事件（扫描带参数二维码事件(用户未关注)）
+                        $resultStr = $this->baseText($postObj, $options->welcome);
+                        break;
+                    case 'unsubscribe'			:   //取消订阅事件
+                        break;
+                    case 'scan'					:   //扫描带参数二维码事件(用户已关注)
+                        break;
+                    case 'location'				:   //上报地理位置事件
+                        break;
+                    case 'click'				:   //自定义菜单事件（点击菜单拉取消息时的事件推送）
+                        $eventkey = strtolower($postObj->EventKey);	    //事件KEY值 转小写
+                        if ($eventkey=="n" || $eventkey=="N") {
+                            $resultStr = $this->newPost($postObj);
+                        }
+                        if ($eventkey=="l" || $eventkey=="L") {
+                            $resultStr = $this->luckyPost($postObj);
+                        }
+                        if ($eventkey=="r" || $eventkey=="R") {
+                            $resultStr = $this->randomPost($postObj);
+                        }
+                        break;
+                    case 'view'					:   //自定义菜单事件（点击菜单跳转链接时的事件推送）
+                        break;
+                    case 'scancode_push'		:   //自定义菜单事件（扫码推事件的事件推送）
+                        break;
+                    case 'scancode_waitmsg'		:   //自定义菜单事件（扫码推事件且弹出“消息接收中”提示框的事件推送）
+                        $ScanType = '';
+                        //扫码类型 ScanType
+                        //扫码结果 ScanResult
+                        switch($ScanType){
+                            case 'qrcode':	//二维码
+                                break;
+                            case 'barcode':	//条码
+                                break;
+                            default :
+                                break;
+                        }
+                        break;
+                    case 'pic_sysphoto'			:   //自定义菜单事件（弹出系统拍照发图的事件推送 ）
+                        break;
+                    case 'pic_photo_or_album'	:   //自定义菜单事件（弹出拍照或者相册发图的事件推送）
+                        break;
+                    case 'pic_weixin'			:   //自定义菜单事件（弹出微信相册发图器的事件推送）
+                        break;
+                    case 'location_select'		:   //自定义菜单事件（弹出地理位置选择器的事件推送）
+                        break;
+                    default						:   //未知事件类型
+                        $resultStr = $this->baseText($postObj, $Event.'未知事件类型，请重新输入！');
+                        break;
                 }
-                if(empty($resultStr)){
-                    $resultStr = $this->baseText($postObj);
+            }else{                         //普通消息
+                switch($msgType){
+                    case 'text'       :   //文本信息
+                        $keyword = trim($postObj->Content);
+                        $cmd = strtolower(substr($keyword, 0, 1));  //转小写
+                        if($cmd=="h"){
+                            $contentStr = "\"n\" 最新日志\n\"r\" 随机日志\n\"l\" 手气不错\n\"s 关键词\" 搜索日志\n\" ";
+                            $resultStr = $this->baseText($postObj, $contentStr);
+                        }elseif ($cmd=="r") {
+                            $resultStr = $this->randomPost($postObj);
+                        }elseif ($cmd=="n") {
+                            $resultStr = $this->newPost($postObj);
+                        }elseif ($cmd=="l") {
+                            $resultStr = $this->luckyPost($postObj);
+                        }elseif ($cmd=="s") {
+                            $searchParam = substr($keyword, 1);
+                            $resultStr = $this->searchPost($postObj, $searchParam);
+                        }else{
+                            $resultStr = $this->baseText($postObj);
+                        }
+                        break;
+                    case 'image'      :   //图片消息
+                        $keyword = '您发送的为图片，链接为:'.$postObj->PicUrl;
+                        break;
+                    case 'voice'      :   //语音消息
+                        $keyword = '您发送的为语音，媒体ID为:'.$postObj->MediaId;
+                        break;
+                    case 'video'      :   //视频消息
+                        $keyword = '您发送的为视频，媒体ID为:'.$postObj->MediaId;
+                        break;
+                    case 'shortvideo' :   //小视频消息
+                        $keyword = '您发送的为小视频，媒体ID为:'.$postObj->MediaId;
+                        break;
+                    case 'location'   :   //地理位置消息
+                        $keyword = '您发送的为地理位置，位置为: '.$postObj->Label.'纬度为: '.$postObj->Location_X.'经度为: '.$postObj->Location_Y;
+                        break;
+                    case 'link'       :   //链接消息
+                        $keyword = '您发送的为链接消息，标题为: '.$postObj->Title.'内容为: '.$postObj->Description.'链接地址为: '.$postObj->Url;
+                        break;
+                    default           :   //未知消息类型
+                        $keyword = $msgType.'未知消息类型！';
+                        break;
                 }
-                echo $resultStr;
+                //普通消息 响应测试
+                if (empty($resultStr) && ($msgType != 'text')){
+                    $resultStr = $this->baseText($postObj, $keyword);
+                }
+            }
+            //唯一响应，切勿删除
+            echo $resultStr;
         }else {
             echo "";
             exit;
